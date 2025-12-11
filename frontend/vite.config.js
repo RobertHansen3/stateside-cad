63981e0b666d22941607163e380a70c74894bc5b
Robert Hansen <skonningm@gmail.com>
2025-12-11 20:36:10 +0100

    modified:   frontend/package-lock.json 	modified:   frontend/vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api": "http://localhost:3000", // backend dev server
+      "/api": "https://stateside-cad.onrender.com/", // backend dev server
     },
   },
 });
