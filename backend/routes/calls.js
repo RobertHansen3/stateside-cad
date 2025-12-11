a98c79a9f55bb3be3ea757c8d716b993b7157004
Robert Hansen <skonningm@gmail.com>
2025-12-11 20:08:56 +0100

    Update of calls.js in backend

--- backend/routes/calls.js
+++ backend/routes/calls.js
@@ -4,7 +4,7 @@ module.exports = (pool) => {
 
   router.get("/", async (req, res) => {
     const q =
-      "SELECT * FROM stateside_calls WHERE state != $1 ORDER BYY created_at DESC";
+      "SELECT * FROM stateside_calls WHERE state != $1 ORDER BY created_at DESC";
     const { rows } = await pool.query(q, ["closed"]);
     res.json(rows);
   });
@@ -13,7 +13,7 @@ module.exports = (pool) => {
     const { caller, phone, coords, notes, required_services, priority } =
       req.body;
     const q = `INSERT INTO stateside_calls (caller, phone, coords, notes, required_services, priority)
-                   VALUES ($1, $2, $3, $4, $5, $6) RETURNING *`;
+               VALUES ($1,$2,$3,$4,$5,$6) RETURNING *`;
     const vals = [
       caller,
       phone || "unknown",
